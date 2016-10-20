1b9a94e0b8c309b8d6540b0801a6aa17c157b97f
Devon Kelly <devon.kelly3@gmail.com>
2016-10-20 16:38:06 -0500

    2nd commit; deosnt work

---
+++ public/js/application.js
@@ -0,0 +1,12 @@
+//= require "vendor/jquery"
+//= require "vendor/jquery.color"
+//= require "vendor/bootstrap-switch"
+//= require "vendor/bootstrap-scrollspy"
+//= require "vendor/bootstrap-dropdown"
+//= require "vendor/bootstrap-button"
+//= require "jquery-sortable"
+//= require_directory "./examples/"
+
+$(function  () {
+  $('ol.cards').sortable();
+});
